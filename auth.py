cf244fcd0e4ca20632ed8aeb6b528e6d87093f82
mark-nicoli <mark21198@gmail.com>
2020-01-13 13:19:50 +0000

    update

--- auth.py
+++ auth.py
@@ -1,18 +1,36 @@
 from InstagramAPI import InstagramAPI
 
-api = InstagramAPI("mark_nicoli19", password)
-api.login
-users_list = []
+def getTotalFollowers(api, user_id):
+    """
+    Returns the list of followers of the user.
+    It should be equivalent of calling api.getTotalFollowers from InstagramAPI
+    """
 
-def getLikes(username):
+    followers = []
+    next_max_id = True
+    while next_max_id:
+        # first iteration hack
+        if next_max_id is True:
+            next_max_id = ''
+
+        _ = api.getUserFollowers(user_id, maxid=next_max_id)
+        followers.extend(api.LastJson.get('users', []))
+        next_max_id = api.LastJson.get('next_max_id', '')
+    return followers
+
+
+if __name__ == "__main__":
+    api = InstagramAPI("mark_nicoli19", password)
     api.login()
-    api.searchUsername(username)
-    result = api.LastJson
-    username_id = result['user']['pk']
-    user_Posts = api.getUserFeed(username_id)
-    result = api.LastJson
-    media_id = result['items'][0]['id']
-    api.getMediaLikers(media_id)
-    users = api.LastJson
-    for users in users:
-        users_list.append({'pk':users['pk'], 'username':users['username']})
+
+    # user_id = '1461295173'
+    user_id = api.username_id
+
+    # List of all followers
+    followers = getTotalFollowers(api, user_id)
+    print('Number of followers:', len(followers))
+
+    # Alternatively, use the code below
+    # (check evaluation.evaluate_user_followers for further details).
+    followers = api.getTotalFollowers(user_id)
+    print('Number of followers:', len(followers))
