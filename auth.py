905813e6effb7c9f60ddd2f5eb20880ebd504c2d
mark-nicoli <mark21198@gmail.com>
2020-01-26 16:43:32 +0000

    update

--- auth.py
+++ auth.py
@@ -1,36 +1,42 @@
+from instapy import InstaPy
+import time
+import pickle
 from InstagramAPI import InstagramAPI
+from datetime import datetime
 
-def getTotalFollowers(api, user_id):
-    """
-    Returns the list of followers of the user.
-    It should be equivalent of calling api.getTotalFollowers from InstagramAPI
-    """
+username = ''
+pwd = ''
+user_id  = ''
 
-    followers = []
-    next_max_id = True
-    while next_max_id:
-        # first iteration hack
-        if next_max_id is True:
-            next_max_id = ''
+API = InstagramAPI(username,pwd)
+API.login()
 
-        _ = api.getUserFollowers(user_id, maxid=next_max_id)
-        followers.extend(api.LastJson.get('users', []))
-        next_max_id = api.LastJson.get('next_max_id', '')
-    return followers
+API.getUsernameInfo(user_id)
+API.LastJson
+followers   = []
+next_max_id = True
+while next_max_id:
+    if next_max_id == True: next_max_id=''
+    _ = API.getUserFollowers(user_id,maxid=next_max_id)
+    followers.extend ( API.LastJson.get('users',[]))
+    next_max_id = API.LastJson.get('next_max_id','')
 
+followers = [dic['username'] for dic in followers]
 
-if __name__ == "__main__":
-    api = InstagramAPI("mark_nicoli19", password)
-    api.login()
+following   = []
+next_max_id = True
+while next_max_id:
+    if next_max_id == True: 
+        next_max_id=''
+    _ = API.getUserFollowings(user_id,maxid=next_max_id)
+    following.extend ( API.LastJson.get('users',[]))
+    next_max_id = API.LastJson.get('next_max_id','')
 
-    # user_id = '1461295173'
-    user_id = api.username_id
+following = [dic['username'] for dic in following]
 
-    # List of all followers
-    followers = getTotalFollowers(api, user_id)
-    print('Number of followers:', len(followers))
+snakes = [x for x in following if x not in followers]
+snakes.reverse()
+print("\n".join(list(snakes)))
+print("Amount of snakes: {}".format(len(snakes)))
 
-    # Alternatively, use the code below
-    # (check evaluation.evaluate_user_followers for further details).
-    followers = api.getTotalFollowers(user_id)
-    print('Number of followers:', len(followers))
+API.logout()
